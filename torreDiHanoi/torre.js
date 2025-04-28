7a3ac99bcd73eb3abee626cc013e0359f9564030
Idlab56 <yute56uwhs@gmail.com>
2025-04-29 00:45:25 +0200

    Add files via upload

---
+++ torreDiHanoi/torre.js
@@ -0,0 +1,13 @@
+const areaDS = document.getElementById('btn1');
+const areaC = document.getElementById('btn2');
+const areaDX = document.getElementById('btn3');
+
+areaDS.addEventListener('click',function(){
+    alert("Dio cane a sinistra");
+})
+areaC.addEventListener('click',function(){
+    alert("Dio cane al centro");
+})
+areaDX.addEventListener('click',function(){
+    alert("Dio cane a destra");
+})
